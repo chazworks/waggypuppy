396910e12e59f161d082ec48cc231229a2578155
Chuck Adams <chuck.adams@pm.me>
2024-10-09 18:34:27 -0600

    gardening: build process simplifications and fixes (#16)
    * refactor(docker): remove cli service and use exec in php service instead
    
    also removes the install of the wordpress-importer plugin in install.js
    
    * fix(docker): add composer 2.7 to php Dockerfile
    
    * test: vendor wordpress-importer plugin instead of downloading it
    
    * refactor(docker): factor Dockerfile into build stages, use nonroot user
    
    * build: replace install.js with meta/bin/install shell script
    
    * fix: rm errant π char
    
    * build: replace all node scripts with shell scripts or direct docker
    
    * build: make test:php just dce phpunit with no ceremony
    
    * style: add WP_SITEURL to wp-tests-config-sample.php and compactify
    
    * feat(build): add 'bootstrap' npm target
    
    * build: stop copying php files out of node_modules because wtf
    
    * build: stop copying any block files at all.

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,3 @@
-const blocksConfig = require( './tools/webpack/blocks' );
 const developmentConfig = require( './tools/webpack/development' );
 const mediaConfig = require( './tools/webpack/media' );
 const packagesConfig = require( './tools/webpack/packages' );
@@ -15,7 +14,6 @@ module.exports = function( env = { environment: "production", watch: false, buil
 	}
 
 	const config = [
-		blocksConfig( env ),
 		...developmentConfig( env ),
 		mediaConfig( env ),
 		packagesConfig( env ),
